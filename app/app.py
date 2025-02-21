f3bc133c1d73fafd04e448242cdc2c4e2cbce96c
Abdallah Jalloh <agjallow@gmail.com>
2025-02-21 15:56:57 +0000

    updated update.html

--- app/app.py
+++ app/app.py
@@ -4,11 +4,15 @@ from flask_migrate import Migrate, migrate
 
 db = SQLAlchemy()
 
+  # Replace this with a strong, unique key
+
 
 def create_app():
     # app = Flask(__name__, template_folder='templates', static_folder='static', static_url_path='/')
     app = Flask(__name__, static_folder='static', static_url_path='/')
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./testdb.db'
+
+    app.secret_key = 'thisismysecretkey'
 
     db.init_app(app)
 
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,4 @@
-from flask import render_template, request
-
+from flask import render_template, request, redirect, url_for, flash
 from models import IICS, GIA
 
 def register_routes(app, db):
@@ -11,13 +10,36 @@ def register_routes(app, db):
             return render_template('index.html', iics=iics)
 
         elif request.method == 'POST':
-            infants =request.form.get('infants')
+            infants = request.form.get('infants')
             diplomats = request.form.get('diplomats')
             deportees = request.form.get('deportees')
 
-            new_iics = IICS(infants=infants, diplomats=diplomats, deportees=deportees)
-            db.session.add(new_iics)
+            new_iic = IICS(infants=infants, diplomats=diplomats, deportees=deportees)
+            db.session.add(new_iic)
             db.session.commit()
 
-            iics = IICS.query.all()
-            return render_template('index.html', iics=iics)
+            flash("New entry added successfully!", "success")
+            return redirect(url_for('index'))
+
+    @app.route('/update/<int:iic_id>', methods=['GET', 'POST'])
+    def update_iic(iic_id):
+        iic = IICS.query.get_or_404(iic_id)
+
+        if request.method == 'POST':
+            iic.infants = request.form.get('infants')
+            iic.diplomats = request.form.get('diplomats')
+            iic.deportees = request.form.get('deportees')
+
+            db.session.commit()
+            flash("Entry updated successfully!", "success")
+            return redirect(url_for('index'))
+
+        return render_template('update.html', iic=iic)
+
+    @app.route('/delete/<int:iic_id>', methods=['POST'])
+    def delete_iic(iic_id):
+        iic = IICS.query.get_or_404(iic_id)
+        db.session.delete(iic)
+        db.session.commit()
+        flash("Entry deleted successfully!", "danger")
+        return redirect(url_for('index'))
